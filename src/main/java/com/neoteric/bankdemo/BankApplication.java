e4832bd16a7325c4cbb46ca42c094a05a6f1a72d
sanja <sanjay.konda24@gmail.com>
2024-07-06 15:04:34 +0530

    bank

---
+++ src/main/java/com/neoteric/bankdemo/BankApplication.java
@@ -0,0 +1,19 @@
+package com.neoteric.bankdemo;
+
+public class BankApplication {
+
+    public String FirstName;
+
+    public String  LastName;
+    public String  NameoftheBank;
+    public String   Address;
+    public long AccountNumber;
+    public long ContactNumber;
+    public int Date;
+
+
+
+
+
+
+}
---
+++ src/test/java/com/neoteric/bankdemo/BankTest.java
@@ -0,0 +1,30 @@
+package com.neoteric.bankdemo;
+
+
+import org.junit.jupiter.api.Test;
+
+public class BankTest {
+
+
+
+    @Test
+
+    public void test(){
+
+
+        BankApplication customer1=new BankApplication();
+
+        customer1.FirstName="sanjay";
+        customer1.LastName="konda";
+        customer1.Address="1-1-526 nehru nagar";
+        customer1.AccountNumber=413441414l;
+        customer1.ContactNumber=99922228882l;
+        customer1.Date=22-2-2022;
+
+
+
+
+    }
+
+
+}
